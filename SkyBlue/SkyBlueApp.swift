cf843359caf0d3d20e5aa2da82976fa316df3d9c
Arshdeep Brar <abrar11@horizon.csueastbay.edu>
2023-01-23 12:17:56 -0800

    Initial Commit

---
+++ SkyBlue/SkyBlueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyBlueApp.swift
+//  SkyBlue
+//
+//  Created by Arshdeep Singh Brar on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SkyBlueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
