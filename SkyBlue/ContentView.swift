905c13c94c720b8d62532bb9487d13e0f087d029
asbrar <89217772+asbrar@users.noreply.github.com>
2023-01-30 11:18:14 -0800

    iOS social media app

--- SkyBlue/ContentView.swift
+++ SkyBlue/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("log_status") var logStatus: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        // MARK: Redirecting User Based on log Status
+        if logStatus{
+            MainView()
         }
-        .padding()
+        else{
+            _LoginView()
+        }
+
     }
 }
 
---
+++ SkyBlue/View/LoadingView.swift
@@ -0,0 +1,23 @@
+//
+//  LoadingView.swift
+//  SkyBlue
+//
+//  Created by Arshdeep Singh Brar on 1/25/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    @Binding var show: Bool
+    var body: some View {
+        ZStack{
+            if show{
+                Group{
+                    Rectangle().fill(.black.opacity(0.25)).ignoresSafeArea()
+                    ProgressView().padding(15).background(.white,in: RoundedRectangle(cornerRadius: 10, style: .continuous))
+                }
+            }
+        }
+        .animation(.easeInOut(duration: 0.25), value: show)
+    }
+}
